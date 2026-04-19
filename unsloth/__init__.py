e7d68a5e215cfcdc797501c548750e042abc2b21
Myo Unabe <myounabe@users.noreply.github.com>
2026-04-19 09:50:27 +0800

    Fix corrupted __init__.py: restore proper header, docstring, and sys import

--- unsloth/__init__.py
+++ unsloth/__init__.py
@@ -5,13 +5,17 @@
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
-# or agreed to in writing, software\n# WITHOUT WARRANTIES OR CONDITIONS OF language governing permissions and
-# limitations under the License.
+# or agreed under the License is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific language governing
+# permissions"""Unsloth - 2x faster LLM finetuning"""
 
-"""Unsloth - 2x faster LLM finetu__version__ = "2024"
+"
 __author__ = ""
-__license__ = "
-#.version_info < (3, 8):
+__license__ = "Apache-2.0"
+
+import sys
+if sys.version_info < (3, 8):
     raise RuntimeError("Unsloth requires Python 3.8 or higher.")
 
 def _check_cuda_available():
